0bd1bf6ebf671300b8eb7a7d77c6695b2a2a6306
RavStark <depond.remi@gmail.com>
2017-10-08 00:48:21 +0200

    New repo GameEngine lib

---
+++ Include/Renderer/Objects/Material.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+typedef int GLint;
+typedef unsigned int GLuint;
+typedef float GLfloat;
+
---
+++ Source/Renderer/LightsManager.cpp
@@ -0,0 +1,37 @@
+#include "Renderer/LightsManager.hpp"
+#include "Renderer/Lights/PointLight.hpp"
+#include "Renderer/Lights/DirectionLight.hpp"
+#include <iostream>
+
+#include "GL\glew.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+LightsManager::LightsManager()
+{
+
+}
+
+LightsManager::~LightsManager()
+{}
+
+void LightsManager::addLight(const std::shared_ptr<PointLight>&pointlight)
+{
+	_pointLights.push_back(pointlight);
+}
+
+void LightsManager::addLight(const std::shared_ptr<DirectionLight>&dirlight)
+{
+	_dirLights.push_back(dirlight);
+}
+
+std::vector<std::shared_ptr<PointLight>> LightsManager::getPointLights() const
+{
+	return _pointLights;
+}
+
+std::vector<std::shared_ptr<DirectionLight>> LightsManager::getDirectionLights() const
+{
+	return _dirLights;
+}
